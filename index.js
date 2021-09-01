bb03d6af8ace07236e5b0df9cf504b50ad17912b
Monjurul Huda Munna <monjurulhuda@gmail.com>
2021-09-01 23:50:22 +0600

    removing mongodb deprecation warning

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if(err) console.log('could not conenct to MongoDB');
+    console.log('successfully connected to MongoDB server');
+});
 
 const app = express();
 
